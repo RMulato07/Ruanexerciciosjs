ef1e2474204f537ca85a6fa4f41ea86678ef30d7
RMulato07 <102757617+RMulato07@users.noreply.github.com>
2022-05-05 15:42:41 -0300

    Add files via upload

---
+++ exerciciosfunsao.js
@@ -0,0 +1,21 @@
+function nome(a){
+    console.log(a);
+}
+nome('ruan mulato');
+
+function soma(a,b){
+    console.log(a+b);
+    return
+}
+soma(17,38);
+ function mutiplicacao(c,d){
+     console.log(c*d)
+     return
+ }
+ mutiplicacao(54,54)
+
+ function media(e,f,g,h){
+     console.log((e+f+g+h)/4)
+     return
+ }
+ media(9,7,8,6)
